69f6209d604db79bf173730eeacfdbcf27af3f94
James Lawn <33882491+JamieTub@users.noreply.github.com>
2021-03-29 21:50:34 +0100

    functionality completed, requires commenting

--- dbHandler.py
+++ dbHandler.py
@@ -1,47 +1,24 @@
 import boto3
-import logging
-from botocore import endpoint
 from botocore.exceptions import ClientError
 
 def create_table(name):
     #accessing the dynamoDB api here
-    db = boto3.client('dynamodb')
+    cloudFormation = boto3.client('cloudformation')
+
+    template_url = "https://s3.us-west-2.amazonaws.com/cloudformation-templates-us-west-2/DynamoDB_Table.template"
 
     try:
-        #creating the database table
-        table = db.create_table(
-            TableName = name,
-            KeySchema = [
-                {
-                    'AttributeName': 'audioclipname',
-                    'KeyType': 'HASH'
-                },
-                {
-                    'AttributeName': 'sentiment',
-                    'KeyType': 'RANGE'
-                }
-            ],
-            AttributeDefinitions=[
-                {
-                    'AttributeName': 'audioclipname',
-                    'AttributeType': 'S'
-                },
-                {
-                    'AttributeName': 'sentiment',
-                    'AttributeType': 'S'
-                }
-            ],
-            Provisionedthroughput = {
-                'ReadCapacityUnits': 5,
-                'WriteCapacityUnits': 5
-            }
+        res = cloudFormation.create_stack(
+            StackName = name,
+            TemplateURL = template_url,
+            Parameters = [{
+                'ParameterKey': 'HashKeyElementName',
+                'ParameterValue': 'trackname'
+            }]
         )
 
-        #allow time for the database table to be created
-        table.meta.client.get_waiter('table_exists').wait(TableName=name)
+        print(name + ' was created.')
+        return res
 
-        print(name + "was created.")
-        return table
-
-    except ClientError as error:
-        logging.error(error)
+    except Exception as err:
+        print("Database already exists.")
--- main.py
+++ main.py
@@ -4,14 +4,18 @@ import boto3
 import S3bucket
 import Queue
 import AudioUpload
+import dbHandler
 
 try:
+    #creating the database
+    dbHandler.create_table("results-table-" + "S1918451")
     #creates and returns a bucket called 'myBucket + studentId'
     S3bucket.createBucket()
     #creates and returns a queue called 'myQueue + studentId'
     Queue.createQueue()
     #add audio file upload to the returned queue
     AudioUpload.fileUpload(S3bucket.getBucketName(),"./res/")
+    
 except:
     print("Error in creation of services")
 
